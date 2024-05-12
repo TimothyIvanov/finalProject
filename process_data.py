96a14c5ef2f79ead80f197eef0328bcbee10f8e7
Timothy Ivanov <ivanovtim101@gmail.com>
2024-05-12 14:51:48 -0700

    Import target_param, remove -999999.0 values

--- process_data.py
+++ process_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from dataretrieval import nwis
 from utils import timer
+from retrieve_sites import target_param
 
 class WaterDataProcessor:
     def __init__(self, filepath, target_param):
@@ -9,12 +9,10 @@ class WaterDataProcessor:
         self.df = self.load_site_data()
     
     @timer
-    #Load site data
     def load_site_data(self):
         return pd.read_csv(self.filepath, dtype={'site_no': str}, low_memory=False)
 
     @timer
-    #Filter out non-numeric columns after certain index
     def filter_columns_by_header(self, numeric_threshold):
         initial_columns = self.df.columns[:numeric_threshold].tolist()
         numeric_headers = [header for header in self.df.columns[numeric_threshold:] if header.isnumeric()]
@@ -22,29 +20,26 @@ class WaterDataProcessor:
         self.df = self.df[selected_columns]
 
     @timer
-    #Remove missing target_param values and select non-empty float columns
     def preprocess_data(self):
+        self.df.replace(-999999.0, pd.NA, inplace=True) #-999999.0 used as NA/error
         self.df = self.df[self.df[self.target_param].notna()]
         float_cols = self.df.select_dtypes(include=['float64']).columns
         self.df = self.df.dropna(subset=[col for col in float_cols if col != self.target_param], how='all')
 
     @timer
-    #Apply a dynamic threshold for column data completeness
     def apply_completeness_threshold(self, threshold_ratio):
-        threshold = self.df.shape[0] / threshold_ratio
+        threshold = self.df.shape[0] * threshold_ratio
         self.df = self.df.dropna(axis=1, thresh=threshold)
 
     @timer
-    #Save filtered dataframe
     def save_data(self, filename):
         self.df.to_csv(filename, index=False)
 
 @timer
-#Filter out non-float data cols from dirty_data.csv, leave only rows with 00010 value and  another feature at given ratio
-def process_data_from_sites():
+def process_data_from_sites(target_param = target_param):
     filepath = 'dirty_data.csv'
-    target_param = '00010'
-    target_to_feature_ratio = 20
+    target_param = target_param
+    target_to_feature_ratio = 0.05
     final_filepath = 'clean_data.csv'
 
     try:
@@ -58,4 +53,3 @@ def process_data_from_sites():
 
 if __name__ == '__main__':
     process_data_from_sites()
-    
