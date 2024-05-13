665d324ee206faf492924101b0bb670bdb9be911
Timothy Ivanov <ivanovtim101@gmail.com>
2024-05-13 13:08:05 -0700

    Add flush functionality

--- utils.py
+++ utils.py
@@ -6,16 +6,16 @@ def timer(func):
         t1 = time()
         result = func(*args, **kwargs)
         t2 = time()
-        print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
+        print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s', flush=True)
         return result
     return wrap_func
 
 def print_status(message, status="in progress"):
     border = "=" * 20
     if status == "in progress":
-        print(f"\n{border}\n{message}...\n{border}\n")
+        print(f"\n{border}\n{message}...\n{border}\n", flush=True)
     elif status == "completed":
-        print(f"\n{border}\n{message} without error!\n{border}")
+        print(f"\n{border}\n{message} without error!\n{border}", flush=True)
 
 def handle_errors(e):
-    print(f'Encountered Error: {e}')
+    print(f'Encountered Error: {e}', flush=True)
